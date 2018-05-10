19e517ea8d4f84b916ab04f778d5716a01d9278f
wangsai <549826282@qq.com>
2018-05-10 13:49:02 +0800

    盛基体育项目初始化

---
+++ sport_backend/src/main/java/com/sjto/config/Constance.java
@@ -0,0 +1,9 @@
+package com.sjto.config;
+
+/**
+ * 常量定义
+ */
+public interface Constance {
+    //为了redis安全，设置项目前缀
+    final String appand = "fuil_wifi_stream_";
+}
---
+++ sport_backend/src/main/java/com/sjto/domain/Card.java
@@ -0,0 +1,87 @@
+package com.sjto.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "t_sport_card")
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Integer type;
+
+    private String bgImgUrl;
+
+    private Integer status;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date updateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getBgImgUrl() {
+        return bgImgUrl;
+    }
+
+    public void setBgImgUrl(String bgImgUrl) {
+        this.bgImgUrl = bgImgUrl;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
