a07a1227c33938c4b091e180273cc7b4732fd316
chenpeng <927388124@qq.com>
2018-05-12 20:44:57 +0800

    场馆配置修改

--- sport_backend/src/main/java/com/sjto/service/VenueConfigService.java
+++ sport_backend/src/main/java/com/sjto/service/VenueConfigService.java
@@ -2,6 +2,9 @@ package com.sjto.service;
 
 import com.sjto.domain.VenueConfigure;
 
+import java.util.List;
+
 public interface VenueConfigService extends BaseService<VenueConfigure, Long>{
 
+    List<VenueConfigure> findVenueConfigure(Long kid, Long oid);
 }
