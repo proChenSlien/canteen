b3e44d00d68ba0df466ac1bf0ea7022047ee6b69
wangsai <549826282@qq.com>
2018-05-15 14:18:45 +0800

    添加后台会员到期天数充值接口

--- sport_backend/src/main/java/com/sjto/controller/VipSingleGymcardInfoController.java
+++ sport_backend/src/main/java/com/sjto/controller/VipSingleGymcardInfoController.java
@@ -6,6 +6,8 @@ import com.sjto.service.BaseService;
 import com.sjto.service.VipSingleGymcardInfoService;
 import com.sjto.utils.Result;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -30,9 +32,9 @@ public class VipSingleGymcardInfoController extends BaseController<VipSingleGymc
 
     @ApiOperation("获取所有的成人会员健身实体卡列表")
     @GetMapping("/query/list")
-    public Result<Map> queryAdultCards(@RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "1") int page){
+    public Result<Map> queryAdultCards(@RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "1") int page, @ModelAttribute VipSingleGymcardInfo entity){
 
-        return vipSingleGymcardInfoService.queryAllVipCardInfoList(page, size);
+        return vipSingleGymcardInfoService.queryAllVipCardInfoList(page, size, entity);
     }
 
     @ApiOperation("获取成人会员健身实体卡信息")
@@ -45,6 +47,15 @@ public class VipSingleGymcardInfoController extends BaseController<VipSingleGymc
     @PostMapping("/verify/{id}")
     public Result<VipSingleGymcardInfoRo> verify(@PathVariable Long id, Integer authState){
         return vipSingleGymcardInfoService.verify(id,authState);
+    }
+
+    @ApiOperation("单人会员健身实体卡到期天数充值")
+    @ApiImplicitParams({ @ApiImplicitParam(dataType = "Long", name = "days", value = "充值的天数"),
+            @ApiImplicitParam(dataType = "Long", name = "userId", value = "用户id")})
+    @PostMapping("/recharge")
+    public Result<VipSingleGymcardInfoRo> recharge(@RequestParam Integer days, @RequestParam Long userId){
+
+        return vipSingleGymcardInfoService.recharge(days, userId);
     }
 
     @Override
